97c4aeb036519a5ec3374b98b051350801342325
Kehong Deng <sevil9955@gmail.com>
2023-06-15 20:42:04 +0200

    Init: reorganize the folders, add react-router and configurate eslint

---
+++ src/components/app/app.component.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Header from '../header/header.component';
+import Home from '../../pages/home/home.page';
+
+import '../../common/styles';
+import './app.component.css';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
